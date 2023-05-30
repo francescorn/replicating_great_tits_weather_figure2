43d715ab9a501259f0fc6ad7ceeb4b99974044e2
Frances Cornwall <113309999+francescorn@users.noreply.github.com>
2023-05-30 15:33:18 -0700

    Update Great_Tits_Survival_D.py

--- Great_Tits_Survival_D.py
+++ Great_Tits_Survival_D.py
@@ -3,8 +3,14 @@ import pandas as pd
 from pandas.tseries.offsets import DateOffset
 import seaborn as sns
 import matplotlib.pyplot as plt
+import requests
 
 # Open file in excel and assign variables
+url = "https://github.com/francescorn/replicating_great_tits_weather_figure2/raw/main/weather_effects_on_great_tits.xlsx"
+response = requests.get(url)
+with open("weather_effects_on_great_tits.xlsx", "wb") as f:
+    f.write(response.content)
+xlsx = pd.ExcelFile("weather_effects_on_great_tits.xlsx")
 df1 = pd.read_excel(xlsx, 'Sheet1')
 df2 = pd.read_excel(xlsx, 'Sheet2')
 df3 = pd.read_excel(xlsx, 'Sheet3')
@@ -19,16 +25,17 @@ df2['Date'] += DateOffset(months = 3)
 df3['Date'] = pd.to_datetime(df3['Tag'], format='%d.%m.%Y')
 
 # Merge datasets and convert data to float
-df = pd.merge(df2, df3, on=['Date'])
+df = pd.merge(df1, df3, on=['Date'])
 
 # Add average max temp per year
 df['NumberFledged'] = df['NumberFledged'].astype(float)
 df['MIND_TA200'] = df['MIND_TA200'].astype(float)
 
 # Graph chart
-fig, ax = sns.lmplot(data = df, x = 'MIND_TA200', y = 'NumberFledged', order=2, line_kws={'color': 'green'})
+ax = sns.lmplot(data = df, x = 'MIND_TA200', y = 'NumberFledged', order=2, line_kws={'color': 'green'})
 ax.set(xlabel='Min. T (C) at ages 6-23 d',
        ylabel='Fledgling Success')
+fig = ax
 file_path = 'd.png'
 fig.savefig(file_path)
 plt.show()
